96c452b0dcb8ba1b8f3fcd5e945c22c2e3db2c05
Matthew Rife <mari242@uky.edu>
2018-04-07 16:42:13 -0400

    Added update so that the speech to text is in a loop

--- Speech.py
+++ Speech.py
@@ -1,16 +1,38 @@
 
 import speech_recognition as sr
 
-r = sr.Recognizer()
+def analyze_text(text):
+    words = text.string()
+    for word in words:
+        print(word)
+    return words
 
-with sr.Microphone() as source:
-    print("Say something")
-    audio = r.listen(source)
-    print("done")
 
-try:
-    print("Google thinks you said " + r.recognize_google(audio))
+def speech_to_text():
+    r = sr.Recognizer()
+    done = False
+    with sr.Microphone() as source:
+        print("Say something")
+        print("done")
+        while done == False:
 
-except:
-    pass
+            audio = r.listen(source)
+            try:
+                text = r.recognize_google(audio)
+                print(text)
+                words = analyze_text(text)
+                for word in words:
+                    print(word + ",")
+                if r.recognize_google(audio) == "done":
+                    done = True
 
+            except:
+                pass
+
+        print("exit")
+
+
+def main():
+    speech_to_text()
+
+main()
